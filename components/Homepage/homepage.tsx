e192ee7225349cde46926bba6dce112bd26f344c
Ibrahim Cheena <143558610+IbrahimCheena@users.noreply.github.com>
2024-04-16 15:01:40 -0700

    Updated the home page

--- components/Homepage/homepage.tsx
+++ components/Homepage/homepage.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
   };
 
   const redirectToPosts = () => {
-    window.location.href = '/posts';
+    window.location.href = '/createpost';
   };
 
   const redirectToRegister = () => {
@@ -26,8 +26,20 @@ export default function HomePage() {
   };
 
   const redirectToProfile = () => {
+    window.location.href = '/profile';
+  };
+
+  const redirectToManage = () => {
     window.location.href = '/manage';
   };
+
+  const redirectToCreatePetProfile = () => {
+    window.location.href = '/createpet';
+  }
+
+  const redirectTOSearch = () => {
+    window.location.href = '/search';
+  }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '15px' }}>
@@ -41,7 +53,11 @@ export default function HomePage() {
         <Button {...buttonStyles} onClick={redirectToPosts}>Posts</Button>
         <Button {...buttonStyles} onClick={redirectToRegister}>Register</Button>
         <Button {...buttonStyles} onClick={redirectToLogin}>Login</Button>
+        <Button {...buttonStyles} onClick={redirectToCreatePetProfile}>Create Pet Profile</Button>
         <Button {...buttonStyles} onClick={redirectToProfile}>Profile</Button>
+        <Button {...buttonStyles} onClick={redirectToManage}>Manage</Button>
+        <Button {...buttonStyles} onClick={redirectTOSearch}>Search</Button>
+        
       </Group>
     </div>
   );
